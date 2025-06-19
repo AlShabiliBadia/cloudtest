81eacc0764ac6221eb2aba47a661e675d1a96f39
AlShabiliBadia <badiaalshabili@gmail.com>
2025-06-19 09:20:30 +0800

    first2

--- db_config.php
+++ db_config.php
@@ -1,8 +1,34 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "password"; 
-$dbname = "project_management_db";
+require '/var/www/html/vendor/autoload.php';
+
+use Aws\SecretsManager\SecretsManagerClient;
+use Aws\Exception\AwsException;
+
+$secretName = 'project/db/credentials';
+$region = 'ap-southeast-1';
+
+$client = new SecretsManagerClient([
+    'version' => 'latest',
+    'region'  => $region,
+]);
+
+try {
+    $result = $client->getSecretValue(['SecretId' => $secretName]);
+} catch (AwsException $e) {
+    error_log($e->getMessage());
+    die("Failed to retrieve database credentials.");
+}
+
+if (isset($result['SecretString'])) {
+    $secret = json_decode($result['SecretString'], true);
+} else {
+    die("SecretString not found.");
+}
+
+$servername = $secret['host'];
+$username   = $secret['username'];
+$password   = $secret['password'];
+$dbname     = 'project_management_db';
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -10,4 +36,3 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
-
